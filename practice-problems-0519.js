85c2006a90b529f69ff8bfc98962e7c6298c1a3c
“Paul <paulkisooshin@gmail.com>
2020-05-19 09:53:41 -0400

    more practice

--- practice-problems-0519.js
+++ practice-problems-0519.js
@@ -25,3 +25,15 @@ function thirdEdge(side1, side2) {
     return (side1 + side2) - 1;
 }
 
+// Create a function that takes in three arguments (prob, prize, pay) and returns true if prob * prize > pay; otherwise return false.
+
+// To illustrate, profitableGamble(0.2, 50, 9) should yield true, since the net profit is 1 (0.2 * 50 - 9), and 1 > 0.
+
+function profitableGamble(prob, prize, pay) {
+    if (prob * prize > pay) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
