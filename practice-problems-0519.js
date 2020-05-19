3dc05e04cddf6f66c6c2ae1f23bef001ae47e58c
“Paul <paulkisooshin@gmail.com>
2020-05-19 09:40:36 -0400

    morning practice 5-19

---
+++ practice-problems-0519.js
@@ -0,0 +1,27 @@
+// MAY 19 2020
+
+// Write a function that converts hours into seconds.
+
+function convertToSeconds(hours) {
+    return hours * 3600;
+}
+
+// Create a function that evaluates an equation.
+
+function eq(evaluate) {
+    return eval(evaluate);
+}
+
+// Create a function that takes height and width and finds the perimeter of a rectangle.
+
+function perimeter(height, width) {
+    return ((height * 2) + (width * 2));
+}
+
+// Create a function that finds the maximum range of a triangle's third edge, where the side lengths are all integers.
+// **(side1 + side2) - 1 = maximum range of third edge.
+
+function thirdEdge(side1, side2) {
+    return (side1 + side2) - 1;
+}
+
