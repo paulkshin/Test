0930527f570b32dbccc950fd820d103c997fb1fe
“Paul <paulkisooshin@gmail.com>
2020-05-22 15:38:56 -0400

    dif format

---
+++ pp-0522.js
@@ -0,0 +1,16 @@
+// Create a function that takes a number as its only argument and returns true if it's less than or equal to zero, otherwise return false.
+
+function lessThanOrEqualToZero(num) {
+    if (num <= 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+// Write a function that returns the length of a string. Make your function recursive.
+function length(str) {
+	return str.length;
+}
+
