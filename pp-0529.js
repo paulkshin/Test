0432fb564d98678075ae220387b521523d23ee72
“Paul <paulkisooshin@gmail.com>
2020-05-29 09:51:55 -0400

    even integer

--- pp-0529.js
+++ pp-0529.js
@@ -1,1 +1,12 @@
 // BEJLWKFJLKJFLKWEJFLF
+
+// Given two integers, a and b, return true if a can be divided evenly by b. Return false otherwise.
+
+function divideEvenly(a, b) {
+    if (Number.isInteger(a / b)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
