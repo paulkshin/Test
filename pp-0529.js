ae24b0a899b8e9cd0e6672db70f769614afb36c3
“Paul <paulkisooshin@gmail.com>
2020-05-29 10:24:14 -0400

    arrow function

--- pp-0529.js
+++ pp-0529.js
@@ -10,3 +10,7 @@ function divideEvenly(a, b) {
         return false;
     }
 }
+
+// Create a function that takes a name and returns a greeting in the form of a string. Don't use a normal function but use an "arrow function".
+
+const helloName = name => "Hello " + name + "!";
