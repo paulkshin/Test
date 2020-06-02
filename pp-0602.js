149b5f629dc67657ad5c1d4c0641d5aea31f2171
“Paul <paulkisooshin@gmail.com>
2020-06-02 19:45:06 -0400

    multiple possibilities regex

--- pp-0602.js
+++ pp-0602.js
@@ -3,3 +3,10 @@
 let myString = "Hello, World!";
 let myRegex = /Hello/;
 let result = myRegex.test(myString);
+
+
+// Use a character class with vowels (a, e, i, o, u) in your regex vowelRegex to find all the vowels in the string quoteSample.
+
+let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
+let vowelRegex = /[aeiou]/gi; // Change this line
+let result = quoteSample.match(vowelRegex); // Change this line
