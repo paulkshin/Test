9b635ceb992f2ce9723c9fa09051edca42cda7ec
“Paul <paulkisooshin@gmail.com>
2020-06-02 19:50:17 -0400

    hyphen regex

--- pp-0602.js
+++ pp-0602.js
@@ -8,5 +8,14 @@ let result = myRegex.test(myString);
 // Use a character class with vowels (a, e, i, o, u) in your regex vowelRegex to find all the vowels in the string quoteSample.
 
 let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
-let vowelRegex = /[aeiou]/gi; // Change this line
-let result = quoteSample.match(vowelRegex); // Change this line
+let vowelRegex = /[aeiou]/gi; /
+let result = quoteSample.match(vowelRegex); 
+
+// Match all the letters in the string quoteSample.
+
+// Note
+// Be sure to match both upper- and lowercase letters.
+
+let quoteSample = "The quick brown fox jumps over the lazy dog.";
+let alphabetRegex = /[a-z]/gi; 
+let result = quoteSample.match(alphabetRegex); 
