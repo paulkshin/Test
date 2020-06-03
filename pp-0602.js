e842967f67be85d3317d75d12b2a71ed840c7da2
“Paul <paulkisooshin@gmail.com>
2020-06-02 20:00:44 -0400

    match numbers and letters

--- pp-0602.js
+++ pp-0602.js
@@ -19,3 +19,10 @@ let result = quoteSample.match(vowelRegex);
 let quoteSample = "The quick brown fox jumps over the lazy dog.";
 let alphabetRegex = /[a-z]/gi; 
 let result = quoteSample.match(alphabetRegex); 
+
+// Create a single regex that matches a range of letters between h and s, and a range of numbers between 2 and 6. 
+// Remember to include the appropriate flags in the regex.
+
+let quoteSample = "Blueberry 3.141592653s are delicious.";
+let myRegex = /[h-s2-6]/gi; 
+let result = quoteSample.match(myRegex);
