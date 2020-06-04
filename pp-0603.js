bf216f3d691d18d84d9c951c7f64ff3011bcec6e
“Paul <paulkisooshin@gmail.com>
2020-06-03 23:10:31 -0400

    odd number problem

---
+++ pp-0603.js
@@ -0,0 +1,7 @@
+// awefaowefja;owifj;
+// Éowyn has written the function isOdd() to check if a given number is odd or not. 
+// Unfortunately, the function does not return the correct result for all the inputs. Help her fix the error.
+
+function isOdd(num) {
+	return num % 2 != 0;
+}
