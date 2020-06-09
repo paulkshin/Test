502077a97e2a592fa0f5e2af1859d33698a073e2
“Paul <paulkisooshin@gmail.com>
2020-06-09 19:51:22 -0400

    more debugging

--- pp-060920.js
+++ pp-060920.js
@@ -8,3 +8,28 @@ console.log(seven + three);
 
 console.log(typeof seven);
 console.log(typeof three);
+
+// one of the most prevalent bugs in coding are syntax or mispelled words. 
+Fix the two pair errors in the code.
+// before:
+let myArray = [1, 2, 3;
+    let arraySum = myArray.reduce((previous, current =>  previous + current);
+    console.log(`Sum of array values is: ${arraySum}`);
+// after
+let myArray = [1, 2, 3];
+let arraySum = myArray.reduce((previous, current) =>  (previous + current));
+console.log("Sum of array values is: ${arraySum}");
+
+
+// Fix the string so it either uses different quotes for the href value, or escape the existing ones. 
+// Keep the double quote marks around the entire string.
+
+// BEFORE
+let innerHtml = "<p>Click here to <a href="#Home">return home</a></p>";
+console.log(innerHtml);
+
+// AFTER
+let innerHtml = "<p>Click here to <a href='#Home'>return home</a></p>";
+console.log(innerHtml);
+
+
