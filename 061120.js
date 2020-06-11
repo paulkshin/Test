932454b71016280b90ec2b5bb68318719f7306f7
“Paul <paulkisooshin@gmail.com>
2020-06-11 16:13:43 -0400

    arrays

---
+++ 061120.js
@@ -0,0 +1,9 @@
+// Using bracket notation, we have now reset the item at index 1 from "b", to "not b anymore".
+
+// In order to complete this challenge, set the 2nd position (index 1) of myArray to anything you want, besides "b".
+
+let myArray = ["a", "b", "c", "d"];
+// Only change code below this line
+myArray[1] = "not b anymore";
+// Only change code above this line
+console.log(myArray);
