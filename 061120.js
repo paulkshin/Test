a3591d17e98de8dba9cc69730bbe27e9fe683e33
“Paul <paulkisooshin@gmail.com>
2020-06-11 16:44:05 -0400

    splice

--- 061120.js
+++ 061120.js
@@ -50,3 +50,19 @@ function popShift(arr) {
   
   console.log(popShift(['challenge', 'is', 'not', 'complete']));
   
+
+//   We've initialized an array arr. Use splice() to remove elements from arr, so that it only contains elements that sum to the value of 10.
+
+// SET UP
+const arr = [2, 4, 5, 1, 7, 5, 2, 1];
+// Only change code below this line
+
+// Only change code above this line
+console.log(arr);
+
+// ANSWER
+const arr = [2, 4, 5, 1, 7, 5, 2, 1];
+// Only change code below this line
+  arr.splice(1, 4);
+// Only change code above this line
+console.log(arr);
