72518de78481d4176aa6e80cbec1a7b55d07fa82
“Paul <paulkisooshin@gmail.com>
2020-06-16 19:27:31 -0400

    slice

--- pp-0616.js
+++ pp-0616.js
@@ -11,3 +11,15 @@ function htmlColorNames(arr) {
   
   console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurquoise', 'FireBrick']));
   
+
+//   We have defined a function, forecast, that takes an array as an argument. Modify the function using slice() to extract information from the argument array and return a new array that contains the elements 'warm' and 'sunny'.
+
+function forecast(arr) {
+    // Only change code below this line
+  
+    return arr.slice(2, 4);
+  }
+  
+  // Only change code above this line
+  console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
+  
