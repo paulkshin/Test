ae2fd285cde7dc9b9c54291eb944f033c78f4d24
“Paul <paulkisooshin@gmail.com>
2020-07-03 18:15:02 -0400

    more stuff

--- 0703.js
+++ 0703.js
@@ -11,3 +11,40 @@ function arrayToString(arr) {
 function calculateExponent(num, exp) {
   return Math.pow(num, exp);
 }
+
+// Write a function to reverse an array.
+
+function reverse(arr) {
+  return arr.reverse();
+}
+
+// Create a function that finds the index of a given item.
+
+function search(arr, item) {
+  return arr.indexOf(item);
+}
+
+// Booleans can also be written as integers, where 1 = True and 0 = False. Make a function that returns the opposite of the boolean given.
+
+function flipBool(b) {
+  if (b) {
+    return 0;
+  }
+  return 1;
+}
+
+// Create a function that takes a string (a random name). If the last character of the name is an "n", return true, otherwise return false.
+
+function isLastCharacterN(word) {
+  if (word.endsWith("n")) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// Create a function that takes an array and a string as arguments and return the index of the string.
+
+function findIndex(arr, str) {
+  return arr.indexOf(str);
+}
