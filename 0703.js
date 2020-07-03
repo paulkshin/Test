c174e5370d5b8e23157631d0ac4da48be798eb35
“Paul <paulkisooshin@gmail.com>
2020-07-03 17:41:48 -0400

    stuff

---
+++ 0703.js
@@ -0,0 +1,13 @@
+// JULY 3 2020
+
+// Create a function that takes an array of numbers or letters and returns a string.
+
+function arrayToString(arr) {
+  return arr.join("");
+}
+
+// Create a function that takes a base number and an exponent number and returns the calculation.
+
+function calculateExponent(num, exp) {
+  return Math.pow(num, exp);
+}
